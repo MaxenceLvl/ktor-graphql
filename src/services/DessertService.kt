d7b7a89113930685b926265e7d5825234761b255
MaxenceLvl <33911862+MaxenceLvl@users.noreply.github.com>
2021-12-15 12:27:34 +0100

    Chapter 3

--- src/services/DessertService.kt
+++ src/services/DessertService.kt
@@ -4,6 +4,7 @@ import com.example.models.Dessert
 import com.example.models.DessertInput
 import com.example.models.DessertsPage
 import com.example.repository.DessertRepository
+import com.example.repository.ReviewRepository
 import com.mongodb.client.MongoClient
 import org.koin.core.KoinComponent
 import org.koin.core.inject
@@ -12,9 +13,12 @@ import java.util.*
 class DessertService: KoinComponent {
     private val client: MongoClient by inject()
     private val repo: DessertRepository = DessertRepository(client)
+    private val reviewRepo: ReviewRepository = ReviewRepository(client)
 
     fun getDessert(id: String): Dessert {
-        return repo.getById(id)
+        val dessert = repo.getById(id)
+        dessert.reviews = reviewRepo.getReviewsByDessertId(id)
+        return dessert
     }
 
     fun getDesserts(): List<Dessert> {
